ca1e01435194aebafe6ac1265f1b6af1a336eb67
tina-aau <qnxau360@gmail.com>
2026-04-07 19:53:12 -0500

    Implement randomize function for grid items
    Added functionality to randomize the appearance of grid items on click.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,28 @@
-// main.js
+let divs = document.querySelectorAll(".grid-container div");
 
-console.log("Hello, World!");
+console.log("page loaded!", divs.length, "words found");
+
+document.addEventListener("click", randomize);
+
+function randomize() {
+  console.log("clicked! scattering words...");
+
+  divs.forEach(function(div) {
+    let rotate     = randomNumber(0, 360);
+    let scale      = randomNumber(50, 150) / 100;
+    let translateX = randomNumber(0, 60);
+    let translateY = randomNumber(0, 60);
+
+    div.style.transform = `rotate(${rotate}deg) scale(${scale}) translate(${translateX}%, ${translateY}%)`;
+    div.style.color = randomColor();
+  });
+}
+
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomColor() {
+  let colors = ["whitesmoke", "dodgerblue", "tomato", "gold", "lightgreen", "violet", "coral"];
+  return colors[randomNumber(0, colors.length - 1)];
+}
